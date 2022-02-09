afb05eb5148c8397f4bb4063ef8b902fab314c60
Tarun Kishore <tarun2kishore@gmail.com>
2022-02-09 14:15:44 +0530

    completed layout

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
+const toggleButton = document.querySelector("button.navToggle");
+const hamburger = document.querySelector("span.hamburger");
+const list = document.querySelector(".list");
 
+toggleButton.onclick = (e) => {
+  list.classList.toggle("list");
+  list.classList.toggle("list-toggled");
 
+  hamburger.classList.toggle("hamburger");
+  hamburger.classList.toggle("hamburger-toggled");
+};
